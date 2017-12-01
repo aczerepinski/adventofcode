3d6b4d8a22e57ac4eb495a10ae5636b42363aa55
Adam Czerepinski <aczerepinski@gmail.com>
2017-12-01 10:07:45 -0500

    Add day 1 part two, circular captcha

--- day1.go
+++ day1.go
@@ -9,17 +9,32 @@ import (
 // The list is circular, so the digit after the last digit is the first digit
 // in the list.
 func Captcha(input string) int {
-	var matches []int
-	for i := 0; i < len(input); i++ {
+	comparisonDistance := 1
+	return doCaptcha(input, comparisonDistance)
+}
+
+// CaptchaTwo finds the sum of digits that match a digit halfway around
+// a circular list
+func CaptchaTwo(input string) int {
+	comparisonDistance := len(input) / 2
+	return doCaptcha(input, comparisonDistance)
+}
+
+func doCaptcha(input string, comparisonDistance int) int {
+	var (
+		matches []int
+		length  = len(input)
+	)
+	for i := 0; i < length; i++ {
 		current := input[i]
-		next := input[(i+1)%len(input)]
+		next := input[(i+comparisonDistance)%length]
 		if current == next {
-			nInt, err := strconv.Atoi(string(current))
+			currentInt, err := strconv.Atoi(string(current))
 			if err != nil {
 				fmt.Printf("could not convert %s to int: %v", string(current), err)
 				return 0
 			}
-			matches = append(matches, nInt)
+			matches = append(matches, currentInt)
 		}
 	}
 	return reduceInts(matches)
--- day1_test.go
+++ day1_test.go
@@ -21,3 +21,22 @@ func TestCaptcha(t *testing.T) {
 		}
 	}
 }
+
+func TestCaptchaTwo(t *testing.T) {
+	tests := []struct {
+		input  string
+		output int
+	}{
+		{"1212", 6},
+		{"1221", 0},
+		{"123425", 4},
+		{"123123", 12},
+		{"12131415", 4},
+	}
+
+	for i, test := range tests {
+		if actual := CaptchaTwo(test.input); actual != test.output {
+			t.Errorf("%d: expected %d, got %d", i, test.output, actual)
+		}
+	}
+}
